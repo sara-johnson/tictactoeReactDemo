40439cf64d0805b0070726d77fa46707944b39b7
sara-johnson <sara-johnson@users.noreply.github.com>
2024-08-06 15:54:09 -0700

    Add ability to set the state of each square

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import { useState } from 'react';
  * The code in App.js creates a component. In React, a component is a piece of reusable code that represents a part of a user interface. Components are used to render, manage, and update the UI elements in your application.
 */
 
-function Square({ value }) {
+function Square() {
+  //value starts off equal to null.
+  const [value, setValue] = useState(null);
+
   function handleClick() {
-    console.log('clicked!');
+    setValue('X');
   }
+  
   return ( <button className="square"
   onClick={handleClick}
   >
@@ -21,19 +25,19 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
      </div>
      <div className="board-row">
-       <Square value="4" />
-       <Square value="5" />
-       <Square value="6" />
+       <Square />
+       <Square />
+       <Square />
      </div>
      <div className="board-row">
-       <Square value="7" />
-       <Square value="8" />
-       <Square value="9" />
+       <Square />
+       <Square />
+       <Square />
      </div>
     </>
   );
